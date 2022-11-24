22e0477a1c84de2f47b4ada463ef10f0596d28ad
pabfam <rypab4@gmail.com>
2022-11-23 22:12:36 -0800

    copied practice work

---
+++ assets/js/script.js
@@ -0,0 +1,128 @@
+// variables for time amd arrays to be used later
+var time = ''
+var timeArray = [];
+var allHours = $("#allHours");
+var calendarEvent = [];
+
+// create a for loop for time and push to calendar
+for (let i = 5; i < 19; i++) {
+    time = i
+    timeArray.push(time);
+}
+
+//create a variable outside of for
+var eventLog = {
+    id: "",
+    eventEl: ""}
+
+//Using jquery to add all elements, classes and ids
+for (let i = 0; i < timeArray.length; i++) {
+    var divParent = $("<div>");
+    var divChild = $("<div>");
+    var textArea = $("<textarea>");
+    var button = $("<button>");
+    var iClass = $("<i>");
+    
+    allHours.append(divParent);
+    divParent.append(divChild);
+    divParent.append(textArea);
+    divParent.append(button);
+    button.append(iClass);
+    
+    divParent.addClass('row time-block')
+    divChild.addClass('col-2 col-md-1 hour text-center py-3')
+    textArea.addClass('col-8 col-md-10 test')
+    button.addClass('btn saveBtn col-2 col-md-1')
+    iClass.addClass('fas fa-save')
+    textArea.attr('id',"description")
+    divParent.attr('data-hour', i+5)
+    
+    // create an if statement to change the color of the boxes
+    if (timeArray[i] == moment().format("HH")) {
+        divParent.addClass("present")
+    } else if (timeArray[i] < moment().format("HH")) {
+        divParent.addClass("past")
+    } else if (timeArray[i] > moment().format("HH")) {
+        divParent.addClass("future")
+    }
+    //create button for saving
+    button.attr('aria-label', "save")
+    iClass.attr('aria-hidden', "true")
+    textArea.attr('rows', '3')
+    
+// format the time correctly
+    if (timeArray[i] == 12) {
+        divChild.text(timeArray[i] + 'pm');
+        
+    } else if (timeArray[i] > 12) {
+        divChild.text(timeArray[i] - 12 + 'pm');
+        timeArray[i] = i - 7
+    } else if (timeArray[i] < 12) {
+        divChild.text(timeArray[i] + 'am');
+        
+    }
+    
+    var description = $('#description')
+    textArea.attr("id", i)
+    
+    
+    // why is timeArray not in normal time
+    var eventLog = {
+        id: i+5,
+        eventEl: description.val()
+        
+    }
+    calendarEvent.push(eventLog)
+    // console.log(description)
+    
+}
+// console.log(calendarEvent)
+
+var currentDate = moment().format('dddd, MMMM Do');
+function getCurrentDate() {
+    $("#currentDay").text(currentDate);
+    }
+
+var saveBtn = $('.saveBtn');
+// console.log(saveBtn)
+
+
+//on click save the text written in the box to the eventEl
+saveBtn.on('click', function(event) {
+    event.preventDefault();
+    var task = $(this).siblings("textarea").val();
+    var timeSlotID = $(this).parent().data('hour');
+    var index = calendarEvent.findIndex(function(obj){
+        return obj.id === timeSlotID
+    })
+    var newEventLog = {
+        id: timeSlotID,
+        eventEl: task 
+    }
+    
+    calendarEvent.splice(index,1,newEventLog)
+    localStorage.setItem('entry',calendarEvent);
+
+
+
+    //  for (let i= 0; i < calendarEvent.length; i++) {
+    //     calendarEvent[i].eventEl = $(this).siblings(`#${i}`).val()
+    // }
+
+    //need to change the current eventEl on this line when pressed save 
+    
+    // console.log(eventEl);
+    // console.log(calendarEvent);
+    // localStorage.setItem("user", JSON.stringify(user));
+    const json = JSON.stringify(calendarEvent);
+    localStorage.setItem('entry', json);
+    // console.log($(this).siblings('#0').val())
+
+//    console.log(calendarEvent) 
+
+})
+
+
+
+
+getCurrentDate()
