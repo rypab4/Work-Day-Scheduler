f831629bfd2069808e2d9f38184bfcfde009f760
pabfam <rypab4@gmail.com>
2022-11-24 10:10:42 -0800

    got the local storage to stay

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,7 +78,7 @@ for (let i = 0; i < timeArray.length; i++) {
     
 }
 
-// add currentDate
+// add currentDate 
 var currentDate = moment().format('dddd, MMMM Do');
 function getCurrentDate() {
     $("#currentDay").text(currentDate);
@@ -98,16 +98,140 @@ saveBtn.on('click', function(event) {
         id: timeSlotID,
         eventEl: task 
     }
-    
-    calendarEvent.splice(index,1,newEventLog)
-    localStorage.setItem('entry',calendarEvent);
-    const json = JSON.stringify(calendarEvent);
-    localStorage.setItem('entry', json);
 
+    //Lines 102-237 should have a better way to write it.  Will rewrite once I learn how.
+    calendarEvent.splice(index,1,newEventLog)
+    console.log(calendarEvent[0].eventEl)
+    const json0 = JSON.stringify(calendarEvent[0].eventEl);
+    const json1 = JSON.stringify(calendarEvent[1].eventEl);
+    const json2 = JSON.stringify(calendarEvent[2].eventEl);
+    const json3 = JSON.stringify(calendarEvent[3].eventEl);
+    const json4 = JSON.stringify(calendarEvent[4].eventEl);
+    const json5 = JSON.stringify(calendarEvent[5].eventEl);
+    const json6 = JSON.stringify(calendarEvent[6].eventEl);
+    const json7 = JSON.stringify(calendarEvent[7].eventEl);
+    const json8 = JSON.stringify(calendarEvent[8].eventEl);
+    const json9 = JSON.stringify(calendarEvent[9].eventEl);
+    const json10 = JSON.stringify(calendarEvent[10].eventEl);
+    const json11 = JSON.stringify(calendarEvent[11].eventEl);
+    const json12 = JSON.stringify(calendarEvent[12].eventEl);
+    const json13 = JSON.stringify(calendarEvent[13].eventEl);
+  
+    
+    localStorage.setItem('entry0', json0);
+    localStorage.setItem('entry1', json1);
+    localStorage.setItem('entry2', json2);
+    localStorage.setItem('entry3', json3);
+    localStorage.setItem('entry4', json4);
+    localStorage.setItem('entry5', json5);
+    localStorage.setItem('entry6', json6);
+    localStorage.setItem('entry7', json7);
+    localStorage.setItem('entry8', json8);
+    localStorage.setItem('entry9', json9);
+    localStorage.setItem('entry10', json10);
+    localStorage.setItem('entry11', json11);
+    localStorage.setItem('entry12', json12);
+    localStorage.setItem('entry13', json13);
+   
 
 })
 
+// need to use getItem to get answers after reloading
+var getEntry0 = localStorage.getItem('entry0'); 
+var getEntry1 = localStorage.getItem('entry1');
+var getEntry2 = localStorage.getItem('entry2');
+var getEntry3 = localStorage.getItem('entry3');
+var getEntry4 = localStorage.getItem('entry4');
+var getEntry5 = localStorage.getItem('entry5');
+var getEntry6 = localStorage.getItem('entry6');
+var getEntry7 = localStorage.getItem('entry7');
+var getEntry8 = localStorage.getItem('entry8');
+var getEntry9 = localStorage.getItem('entry9');
+var getEntry10 = localStorage.getItem('entry10');
+var getEntry11 = localStorage.getItem('entry11');
+var getEntry12 = localStorage.getItem('entry12');
+var getEntry13 = localStorage.getItem('entry13');
 
+if (getEntry0 !== null){
+    var getEntry0 = getEntry0.replace(/["]/g,'')
+}else{ 
+    var getEntry0 = ""
+}
+if (getEntry1 !== null){
+    var getEntry1 = getEntry1.replace(/["]/g,'')
+}else{ 
+    var getEntry1 = ""
+}
+if (getEntry2 !== null){
+    var getEntry2 = getEntry2.replace(/["]/g,'')
+}else{ 
+    var getEntry2 = ""
+}
+if (getEntry3 !== null){
+    var getEntry3 = getEntry3.replace(/["]/g,'')
+}else{ 
+    var getEntry3 = ""
+}
+if (getEntry4 !== null){
+    var getEntry4 = getEntry4.replace(/["]/g,'')
+}else{ 
+    var getEntry4 = ""
+}
+if (getEntry5 !== null){
+    var getEntry5 = getEntry5.replace(/["]/g,'')
+}else{ 
+    var getEntry5 = ""
+}
+if (getEntry6 !== null){
+    var getEntry6 = getEntry6.replace(/["]/g,'')
+}else{ 
+    var getEntry6 = ""
+}
+if (getEntry7 !== null){
+    var getEntry7 = getEntry7.replace(/["]/g,'')
+}else{ 
+    var getEntry7 = ""
+}
+if (getEntry8 !== null){
+    var getEntry8 = getEntry8.replace(/["]/g,'')
+}else{ 
+    var getEntry8 = ""
+}
+if (getEntry9 !== null){
+    var getEntry9 = getEntry9.replace(/["]/g,'')
+}else{ 
+    var getEntry9 = ""
+}
+if (getEntry10 !== null){
+    var getEntry10 = getEntry10.replace(/["]/g,'')
+}else{ 
+    var getEntry10 = ""
+}
+if (getEntry11 !== null){
+    var getEntry11 = getEntry11.replace(/["]/g,'')
+}else{ 
+    var getEntry11 = ""
+}if (getEntry12 !== null){
+    var getEntry12 = getEntry12.replace(/["]/g,'')
+}else{ 
+    var getEntry12 = ""
+}if (getEntry13 !== null){
+    var getEntry13 = getEntry13.replace(/["]/g,'')
+}else{ 
+    var getEntry13 = ""
+}
+$('#0').val(getEntry0)
+$('#1').val(getEntry1)
+$('#2').val(getEntry2)
+$('#3').val(getEntry3)
+$('#4').val(getEntry4)
+$('#5').val(getEntry5)
+$('#6').val(getEntry6)
+$('#7').val(getEntry7)
+$('#8').val(getEntry8)
+$('#9').val(getEntry9)
+$('#10').val(getEntry10)
+$('#11').val(getEntry11)
+$('#12').val(getEntry12)
+$('#13').val(getEntry13)
 
-
-getCurrentDate()
